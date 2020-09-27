6625e3f05067935b4d0d0ed800903b3ee90582ee
AlexKovalenkoBY <kovalenkoam@gmail.com>
2020-09-27 20:33:13 +0300

    feat: sol dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -3,4 +3,11 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function createDreamTeam(/* members */) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  function createDreamTeam (teamList) {
+    let teamName = "";
+    for ( let i=0; i<teamList.length; i++ ){
+      teamName.push(teamList[i][1].toUppercase());
+    }
+    return teamName.sort();
+  }
 };
