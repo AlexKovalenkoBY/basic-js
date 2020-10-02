e932cc54d3d0e6ccbaf11985ff2eca2d8df151f2
AlexKovalenkoBY <kovalenkoam@gmail.com>
2020-10-02 21:16:49 +0300

    fix: partialy fixed errors on task getseason

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,21 +2,16 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(cats_Array) {
-    /*countCats([ [0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2] ]) => 3*/
-
-    let cats = 0;
-
-    let cat = "^^";
-
-    for (let i = 0; i < cats_Array.length; i++) {
-        for (let j = 0; j < cats_Array[i].length; j++) {
-          
-            if (cats_Array[i][j] === cat) {
-                cats = cats + 1;
-            }
-            
-        }
+  /*countCats([ [0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2] ]) => 3*/
+  let cats = 0;
+  let cat = "^^";
+  for (let i = 0; i < cats_Array.length; i++) {
+    for (let j = 0; j < cats_Array[i].length; j++) {
+      if (cats_Array[i][j] === cat) {
+        cats = cats + 1;
+      }
     }
+  }
 
-    return cats;
+  return cats;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,15 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  function getSeason (date) {
-    let answer = "";
-    let month = date.getMonth();
-    if ((month === 11) || (month === 0) || (month === 1) ) {answer = "winter"};
-    if ((month >1) && (month < 6 )  ) {answer = "spring"};
-    if ((month >5) && (month < 9 )  ) {answer = "summer"};
-    if ((month >8) && (month < 12 )  ) {answer = "autumn"};
-    return answer;
-
+module.exports = function getSeason(myDate) { ///не работает тест с фэйковой датой. надо искать причину
+  //throw new CustomError('Not implemented');
+  if (myDate === null || myDate === undefined) {
+    return "Unable to determine the time of year!";
   }
+  if (myDate instanceof Date) {
+    let answer = "";
+
+    let month = myDate.getMonth();
+
+    if (month === 11 || month === 0 || month === 1) {
+      answer = "winter";
+    }
+    if (month > 1 && month < 5) {
+      answer = "spring";
+    }
+    if (month > 4 && month < 8) {
+      answer = "summer";
+    }
+    if (month > 7 && month < 11) {
+      answer = "autumn";
+    }
+    return answer;
+  } else {return 'THROWN'};
+  
 };
