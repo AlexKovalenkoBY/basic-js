93f626192673fb233375b5c2bfe7bb8893844dd4
Alexandr Kovalenko <kovalenkoam@gmail.com>
2020-10-02 13:27:50 +0300

    fix: fixed errors and run all tests

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,25 +1,22 @@
+/* eslint-disable no-unused-vars */
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
- // throw new CustomError('Not implemented');
-  
-  function countCats (cats_Array) {
+module.exports = function countCats(cats_Array) {
     /*countCats([ [0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2] ]) => 3*/
-    let cats = 0;
-    
-    let cat = '^^';
-    for (let i=0;i<cats_Array.length; i++){
-      for (let j=0; j < cats_Array[i].length;j++)
-      { if ( cats_Array[i][j].toString()==cat.toString() ) {
-        console.log(""+i+";"+j);
-        cats = cats+1;
 
-      }
-    } 
+    let cats = 0;
+
+    let cat = "^^";
+
+    for (let i = 0; i < cats_Array.length; i++) {
+        for (let j = 0; j < cats_Array[i].length; j++) {
+          
+            if (cats_Array[i][j] === cat) {
+                cats = cats + 1;
+            }
+            
+        }
     }
 
     return cats;
-
-  }
-  
 };
